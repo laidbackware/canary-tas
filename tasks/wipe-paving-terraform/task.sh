c7ea24fe26038987f48490c7cd44f0046b2a2352
Matt Proud <proudmatt@gmail.com>
2020-11-06 12:24:28 +0000

    fix tf env vars

--- tasks/wipe-paving-terraform/task.sh
+++ tasks/wipe-paving-terraform/task.sh
@@ -9,9 +9,7 @@ cp ${ROOT_DIR}/terraform/terraform-* /usr/local/bin/terraform
 chmod +x /usr/local/bin/terraform
 
 ENV_FILE=vars/${FOUNDATION}/env/env.yml
-OM_TARGET=$(bosh int config/${ENV_FILE} --path /target | \
-                            sed s"/((domain))/${OM_VAR_domain}/" | \
-                            sed s"/((foundation))/${OM_VAR_foundation}/")
+OM_TARGET=$(om interpolate -c config/${ENV_FILE} -s --path /target) || exit 1
 
 if $(curl -k --output /dev/null --silent --head --fail -m 5 ${OM_TARGET})
 then
@@ -24,7 +22,7 @@ export TF_VAR_access_key="$(bosh int <(echo ${CLOUD_CREDS}) --path /client_id)"
 export TF_VAR_secret_key="$(bosh int <(echo ${CLOUD_CREDS}) --path /client_secret)"
 
 terraform init -backend-config="bucket=${STATE_BUCKET}" \
-    -backend-config="key=${FOUNDATION}/terraform.tfstate" \
+    -backend-config="key=${OM_VAR_foundation}/terraform.tfstate" \
     -backend-config="endpoint=${S3_ENDPOINT}"\
     -backend-config="access_key=${STATE_BUCKET_KEY_ID}"\
     -backend-config="secret_key=${STATE_BUCKET_SECRET_KEY}"
